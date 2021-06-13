89805e69ffcdae1594915fc049d90f043bb9348f
priscillahuynh <priscillamhuynh@gmail.com>
2021-06-13 06:13:05 +0400

    add actions to books controller and modify route to nest books under shelf

--- app/controllers/concerns/api/v1/books_controller.rb
+++ app/controllers/concerns/api/v1/books_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::BooksController < ApplicationController
+    before_action :set_shelf
+
+    def index
+        @books = @account.books
+        render json: @books
+    end
+
+    def create
+        @book = Book.new(book_params)
+        if @book.save
+            render json: @book 
+        else
+            render json: {error: "Error creating Book"}
+        end
+    end
+
+    def show 
+        @book = Book.find(params[:id])
+        render json: @book 
+    end
+
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+    end
+
+
+    private 
+
+    def set_shelf
+        @shelf = Shelf.find(params[:shelf_id])
+    end
+
+    def book_params
+        params.require(:book).permit(:title, :author, :image_url, :shelf_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do 
     namespace :v1 do
-      resources :books  
-      resources :shelves
+      resources :shelves do 
+        resources :books  
+      end
     end
   end
 end
